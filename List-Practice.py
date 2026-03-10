98a548cb090efff108e971eea90b2bb685eee141
deeznuts <deeznuts@gmail.com>
2026-03-10 18:15:02 -0400

    123

---
+++ List-Practice.py
@@ -0,0 +1,12 @@
+
+
+# shop_cart = ['apple', 'banana', 'orange']
+# print(shop_cart[0::2])
+
+list = [1, 2, 3, 4, 5]
+list.append(100)
+list.insert(0, 992)
+deezer = list
+list.pop(0  )
+print(list)
+print(deezer)
