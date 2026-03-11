5e95fa8d4793f41dcc29e83cfa55f48231c0e650
deeznuts <deeznuts@gmail.com>
2026-03-11 12:16:22 -0400

    practicing more

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -3,27 +3,49 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+# password = []
+
+# lettercount = int(input("how many letters do you want in your password? : \n"))
+# numbercount = int(input("how many numbers do you want in your password? : \n"))
+# symbolcount = int(input ("how many symbols do you want in your password? : \n"))
+
+# for _ in range(lettercount):
+#     password.append(random.choice(letters))
+# for _ in range(numbercount):
+#     password.append(random.choice(numbers)) 
+# for _ in range(symbolcount):
+#     password.append(random.choice(symbols))
+
+# random.shuffle(password)
+
+# # Way ONE
+# # password_list = "".join(password)
+# # print(password_list)
+
+# # WAY TWO
+# password_list = ""
+# for char in password:
+#     password_list += char
+# print (password_list)
+
 password = []
 
 lettercount = int(input("how many letters do you want in your password? : \n"))
 numbercount = int(input("how many numbers do you want in your password? : \n"))
 symbolcount = int(input ("how many symbols do you want in your password? : \n"))
 
-for _ in range(lettercount):
+for _ in range (lettercount):
     password.append(random.choice(letters))
-for _ in range(numbercount):
-    password.append(random.choice(numbers)) 
-for _ in range(symbolcount):
+for _ in range (lettercount):
+    password.append(random.choice(numbers))
+for _ in range (symbolcount):
     password.append(random.choice(symbols))
 
 random.shuffle(password)
 
-# Way ONE
-# password_list = "".join(password)
-# print(password_list)
-
-# WAY TWO
-password_list = ""
+password_str= ""
 for char in password:
-    password_list += char
-print (password_list)
+    password_str+= char
+
+
+print(f"this is your password bro : {password_str}")
